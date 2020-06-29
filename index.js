cb6737b5d9c5be2ee7420836f2688e500d16bce4
AlvarDev <alvardev.lp@gmail.com>
2020-06-28 22:31:54 -0300

    Cloud Storage download

--- index.js
+++ index.js
@@ -1,10 +1,14 @@
 const bodyParser = require("body-parser");
 const express = require('express');
 const path = require("path");
-const tmp = require("tmp");
+const os = require('os');
+const fs = require('fs');
 
 const app = express();
 const PORT = process.env.PORT || 8080;
+
+const { Storage } = require("@google-cloud/storage");
+const storage = new Storage();
 
 function validateReq(req, res, next){
   if (!("image" in req.body)) {
@@ -15,7 +19,36 @@ function validateReq(req, res, next){
   next();
 }
 
-function getPoses(req, res){
+async function getPoses(req, res){
+  
+  // Getting Image path, Cloud use a Regex
+  const attrs = req.body.image.split('/');
+  const bucketName = attrs[2];
+  const filename = attrs[attrs.length - 1];
+  const imageGCS = req.body.image.replace(`gs://${bucketName}/`, "");
+  const imagePath = path.join(os.tmpdir(), filename);
+
+  console.log(`
+    bucketName: ${bucketName}
+    fileName: ${filename}
+    imageCGS: ${imageGCS}
+    imagePath: ${imagePath}
+  `);
+
+  // Downlaod from GCS
+  try {
+    await storage
+      .bucket(bucketName)
+      .file(imageGCS)
+      .download({ destination: imagePath });
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(404).send({ message: "File not found" });
+    return;
+  }
+
+  fs.unlinkSync(imagePath);
   res.status(200).send({sucess: true});
 }
 
